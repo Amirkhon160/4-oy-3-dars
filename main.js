957f434777b434fb08209db7387a4bb7952e7666
Amirkhon160 <amirkhoninagamov001@gmail.com>
2024-07-21 17:03:28 +0500

    uyga vazifa

---
+++ main.js
@@ -0,0 +1,25 @@
+//let num = prompt(`eng katta son`)
+//let number = Math.max
+
+//console.log(Math.max);
+
+//let random = Math.round(Math.random() * 100) * 2;
+
+//let savol = random * 2
+
+//console.log(random)
+//console.log(savol)
+
+let random = Math.round(Math.random() * 100) * 2;
+
+let num = random * 2;
+
+let split = num / 2
+
+let num2 = split % 3
+
+const savol = alert(`Hozir random soni chiqadi`)
+alert(`random son ${random}`)
+const javob = alert(`randomdan chiqqan son ${random} \n biz uni 2ga kopaytirib ${num} \n shu sonni 2 ga bolib ${split} \n va shu sonni 3ga bolganda qoldig'i ${num2}`)
+
+
